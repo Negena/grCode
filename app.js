106f6304384046d063b38a91dab94a78847cc3b6
Negena <justnegena@gmail.com>
2022-03-27 21:23:36 +0500

    ready to push

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require("express");
 const app = express();
 const ejs = require("ejs");
 const qrcode = require("qrcode");
-
+const fs = require("fs");
+const id = require("shortid");
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
@@ -14,13 +15,29 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
-app.post("/scan", (req,res) => {
+app.post("/scan", async(req,res) => {
   const input = req.body.text;
-  qrcode.toDataURL(input, (err, src)  => {
-    res.render("scan", {qr_src: src})
+
+  await qrcode.toDataURL(input, (err, src)  => {
+    let ids = id.generate();
+
+    fs.writeFile(`user${ids}.pdf`, `decoding is: ${input}, SOURCES is ${src}`, function(errs, data) {
+      if (errs) throw errs;
+      console.log(data)
+     res.render("scan", {qr_src: src, input, data, ids})
+    })
   })
 });
 
+
+
+
+
+app.get("/user/:id", async(req, res) => {
+  const file = `${__dirname}/user${req.params.id}.pdf`
+  await res.download(file)
+  res.redirect('/')
+})
 
 
 app.listen(3000, () => {
